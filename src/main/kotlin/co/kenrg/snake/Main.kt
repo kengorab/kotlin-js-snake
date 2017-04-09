74e72f0bcf8dc7eef57ed85dca38a38338852ca7
Ken Gorab <ken.gorab@gmail.com>
2017-04-09 10:06:22 -0400

    Display an h1 tag using kotlinx-html. Removed package.json and yarn.lock files, since we will be referencing the javascript files from the installed jars instead of installing them from npm.

--- src/main/kotlin/co/kenrg/snake/Main.kt
+++ src/main/kotlin/co/kenrg/snake/Main.kt
@@ -1,5 +1,11 @@
 package co.kenrg.snake
 
+import kotlinx.html.dom.append
+import kotlinx.html.js.h1
+import kotlin.browser.document
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    document.body?.append {
+        h1 { +"🐍 Snake" }
+    }
 }
